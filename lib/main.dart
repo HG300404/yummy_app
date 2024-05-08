86a796e44b987706b322695d137e315505326188
Your Name <you@example.com>
2024-05-08 22:58:30 +0700

    Set up project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      // home: DetailDish(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
